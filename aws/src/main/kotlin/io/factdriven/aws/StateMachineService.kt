6ce999d03265d8149b866d2648d048d8c560e07d
aseik <alexander.t.seik@gmail.com>
2020-03-12 14:12:49 +0100

    Add example dummy xor for step functions

--- aws/src/main/kotlin/io/factdriven/aws/StateMachineService.kt
+++ aws/src/main/kotlin/io/factdriven/aws/StateMachineService.kt
@@ -4,14 +4,31 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder
 import com.amazonaws.services.stepfunctions.builder.StateMachine
 import com.amazonaws.services.stepfunctions.builder.StepFunctionBuilder
+import com.amazonaws.services.stepfunctions.builder.StepFunctionBuilder.end
+import com.amazonaws.services.stepfunctions.builder.StepFunctionBuilder.next
+import com.amazonaws.services.stepfunctions.builder.conditions.Condition
+import com.amazonaws.services.stepfunctions.builder.conditions.NumericEqualsCondition
+import com.amazonaws.services.stepfunctions.builder.states.Choice
+import com.amazonaws.services.stepfunctions.builder.states.ChoiceState
 import com.amazonaws.services.stepfunctions.model.CreateStateMachineRequest
 import com.amazonaws.services.stepfunctions.model.UpdateStateMachineRequest
 
 class StateMachineService {
     fun createDummyStateMachineDefinition() : StateMachine {
         return StepFunctionBuilder.stateMachine()
-                .startAt("Example")
+                .startAt("a")
+                .state("a", ChoiceState.builder().choices(
+                        Choice.builder().condition(NumericEqualsCondition.builder().expectedValue(0L).variable("$.output.my"))
+                                .transition(next("Example")),
+                        Choice.builder().condition(NumericEqualsCondition.builder().expectedValue(1L).variable("$.output.my"))
+                                .transition(next("Example2"))
+                ))
                 .state("Example",
+                        StepFunctionBuilder.taskState()
+                                .resource("arn:aws:lambda:REGION:ACCOUNT_ID:function:FUNCTION_NAME")
+                                .transition(StepFunctionBuilder.end())
+                )
+                .state("Example2",
                         StepFunctionBuilder.taskState()
                                 .resource("arn:aws:lambda:REGION:ACCOUNT_ID:function:FUNCTION_NAME")
                                 .transition(StepFunctionBuilder.end())
@@ -27,6 +44,7 @@ class StateMachineService {
 
         val updateArn: String?
         val stepFunctionRoleArn = System.getenv("STEP_FUNCTION_ROLE_ARN")
+        println(stepFunctionRoleArn)
 
         try {
             val resultCreate = client.createStateMachine(CreateStateMachineRequest()
