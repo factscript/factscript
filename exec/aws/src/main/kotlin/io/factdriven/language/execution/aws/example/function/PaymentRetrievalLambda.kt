f799d72accd98c8cfac4f48d40f7a8b87bd94fd5
alexanderseik <alexander.t.seik@gmail.com>
2020-04-30 16:22:40 +0200

    Refactor Lambda

---
+++ exec/aws/src/main/kotlin/io/factdriven/language/execution/aws/example/function/PaymentRetrievalLambda.kt
@@ -0,0 +1,13 @@
+package io.factdriven.language.execution.aws.example.function
+
+import io.factdriven.aws.lambda.FlowlangLambda
+import io.factdriven.language.Flows
+import io.factdriven.language.definition.Flow
+import io.factdriven.language.execution.aws.example.function.PaymentRetrieval
+
+class PaymentRetrievalLambda : FlowlangLambda() {
+    override fun definition(): Flow {
+        PaymentRetrieval.init()
+        return Flows.get(PaymentRetrieval::class)
+    }
+}
