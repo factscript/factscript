15f788929a83c467bbc4d082441f662f566c77d7
Martin Schimak <martin.schimak@plexiti.com>
2020-05-05 10:43:19 +0200

    Slightly adapt maximum length of label lines.

--- lang/src/main/kotlin/io/factdriven/language/impl/utils/String.kt
+++ lang/src/main/kotlin/io/factdriven/language/impl/utils/String.kt
@@ -4,8 +4,8 @@ fun String.toSentenceCase() = "(.)([A-Z\\d])".toRegex().replace(this) {
     "${it.groupValues[1]} ${it.groupValues[2].toLowerCase()}"
 }
 
-fun String.asLines(length: Int = 12) = "(.{1,$length})(?:\\s|$)".toRegex().replace(this) {
+fun String.asLines(length: Int = 15) = "(.{1,$length})(?:\\s|$)".toRegex().replace(this) {
     "${it.groupValues[1]}\n"
 }.trim()
 
-fun String.toLines(length: Int = 12) = asLines(length).lines()
+fun String.toLines(length: Int = 15) = asLines(length).lines()
